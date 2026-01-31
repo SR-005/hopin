8ae6af91a3f47d22de034adc671901a5127006a7
Sreeram V Gopal <sreeramvg100@gmail.com>
2026-01-31 23:19:42 +0530

    templates test

--- hopin_app/views.py
+++ hopin_app/views.py
@@ -4,7 +4,7 @@ from django.contrib import messages
 # Create your views here.
 
 def login(request):
-    return render(request,"login.html")
+    return render(request,"testlogin.html")
 
 def signup(request):
     return render(request,"signup.html")
