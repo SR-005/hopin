d2a35c949daae92e7f59084667b560290cb2be84
Sreeram V Gopal <sreeramvg100@gmail.com>
2026-02-24 12:57:16 +0530

    spacial computation done

--- hopin_app/ml/routeopt.py
+++ hopin_app/ml/routeopt.py
@@ -1,22 +1,19 @@
 import math
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
+from haversine import haversine, Unit
 
 sematicweight=0.6
 sematicheight=0.2
 
 #Haversine's Distance Function
-def distance(latitude1,longitude1,latitude2,longitude2):
-    earthradius=6371        #radius of the earth
+def spacialscore(location1,location2):
+    distance=haversine(location1, location2)
+    spatialscore=1/(1+distance)
+    return spatialscore
 
-
-    latitude1radius=math.radians(latitude1)
-    longitude1radius=math.radians(longitude1)
-    latitude2radius=math.radians(latitude2)
-    longitude2radius=math.radians(longitude2)
-
-    latitudediff=latitude2radius-latitude1radius
-    longitudediff=longitude2radius-longitude1radius
-
-
+def finalscore(riderroute,riderlocation,availabletrips):
+    allroutes=[riderroute]
+    for trip in availabletrips:
+        allroutes(trip)
     return 0
