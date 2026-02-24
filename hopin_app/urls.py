c617c164fc90bf607eaa4210c892313434784069
Sreeram V Gopal <sreeramvg100@gmail.com>
2026-02-24 20:16:30 +0530

    minor changes

--- hopin_app/urls.py
+++ hopin_app/urls.py
@@ -4,8 +4,7 @@ from . import views
 urlpatterns = [
     path("", views.landingfunction, name="landing"),
     path("testlogin/", views.testloginfunction, name="testlogin"),  # test login
-    path("testsignup/", views.testsignupfunction,
-         name="testsignup"),  # test signup
+    path("testsignup/", views.testsignupfunction,name="testsignup"),  # test signup
     path("testcreatetrip/", views.testdriverfunction, name="testdriver"),
 
     path("login/", views.loginfunction, name="login"),
