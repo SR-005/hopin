9bd79da2573e606147ba0a05fd64ce3eac70ac37
Sreeram V Gopal <sreeramvg100@gmail.com>
2026-03-22 20:19:18 +0530

    rider,tracking and location views

--- hopin_app/urls.py
+++ hopin_app/urls.py
@@ -1,31 +1,38 @@
 from django.urls import path
 from . import views
-from .views import authview,payandrateview,driverview
+from .views import authview,payandrateview,driverview,riderview,trackingview,locationview
 
 urlpatterns=[
 
-    #auth view function
+    #authentication view functions
     path("", authview.landingfunction, name="landing"),
     path("login/", authview.loginfunction, name="login"),
     path("signup/", authview.signupfunction, name="signup"),
     path("verify/", authview.verifyfunction, name="verify"),
     path("logout/", authview.logoutfunction, name="logout"),
 
-    #payment and rating view function
+
+    #payment and rating view functions
     path("testpay/<int:paymentid>/", payandrateview.testpayfunction, name="testpay"),
     path("verifypayment/", payandrateview.verifypayment, name="verifypayment"),
 
-    #driver view function
+
+    #driver view functions
     path("testcreatetrip/", driverview.testdriverfunction, name="testdriver"),   #test driver
 
 
+    #rider view functions
+    #path("testrider/", riderview.testriderfunction, name="testrider"),   #test rider
+    path("rider/", riderview.riderfunction, name="rider"),
+
+
+    #live location view functions
+    path("testlocation/<int:rideid>/", locationview.testlocationfunction, name="testlocation"),   #test live location
+    path("updatelocation/<int:rideid>/", locationview.updatelocation, name="update_location"),
     
-    path("testrider/", views.testriderfunction, name="testrider"),   #test rider
-    path("testlocation/<int:rideid>/", views.testlocationfunction, name="testlocation"),   #test live location
-    path("testtracking/<int:rideid>/", views.testtrackingfunction, name="testtracking"),   #test tracking
-    path("rider/", views.riderfunction, name="rider"),
-    path("updatelocation/<int:rideid>/", views.updatelocation, name="update_location"),
-    path("fetchtracking/<int:rideid>/", views.fetchtracking, name="fetchtracking"),   #test tracking
-    path("fetchstatus/<int:requestid>/", views.fetchstatus, name="fetchstatus"),    #fetch status of ride: completed or not
+    #location tracking view functions
+    path("testtracking/<int:rideid>/", trackingview.testtrackingfunction, name="testtracking"),   #test tracking
+    path("fetchtracking/<int:rideid>/", trackingview.fetchtracking, name="fetchtracking"),   #test tracking
+    path("fetchstatus/<int:requestid>/", trackingview.fetchstatus, name="fetchstatus"),    #fetch status of ride: completed or not
     
 ]
